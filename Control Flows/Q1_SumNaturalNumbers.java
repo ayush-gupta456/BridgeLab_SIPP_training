29df09f1bc5ff42305dd54cb92a336061b7fc672
AYUSH GUPTA <omayush456.ag@gmail.com>
2025-06-13 09:36:50 +0530

    Sum of natural numbers

---
+++ Control Flows/Q1_SumNaturalNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Q1_SumNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0) {
+            int sum = 0, i = 1;
+            while (i <= n) {
+                sum += i;
+                i++;
+            }
+            int formulaSum = n * (n + 1) / 2;
+            System.out.println("Sum using loop: " + sum);
+            System.out.println("Sum using formula: " + formulaSum);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
