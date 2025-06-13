6af7e6aaeb7ca6f00fed011f15cf02593774a941
AYUSH GUPTA <omayush456.ag@gmail.com>
2025-06-13 09:45:26 +0530

    CountDigits

---
+++ Control Flows/Q32_CountDigits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Q32_CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt(), count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        System.out.println("Digits: " + count);
+    }
+}
