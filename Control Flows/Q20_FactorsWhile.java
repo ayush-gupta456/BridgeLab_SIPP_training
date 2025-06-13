d3887fed16e82a75a48595d67132d746ba564583
AYUSH GUPTA <omayush456.ag@gmail.com>
2025-06-13 09:42:47 +0530

    FactorsWhile

---
+++ Control Flows/Q20_FactorsWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Q20_FactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt(), i = 1;
+        if (number > 0) {
+            while (i < number) {
+                if (number % i == 0)
+                    System.out.println(i);
+                i++;
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+}
