d6c9e038e009f63e71f75fddfe7aa11b457475ca
AYUSH GUPTA <omayush456.ag@gmail.com>
2025-06-13 09:40:38 +0530

    SumNaturalCheck

---
+++ Control Flows/Q11_SumNaturalCheck.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Q11_SumNaturalCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 0)
+            System.out.println("The sum of " + n + " natural numbers is " + (n * (n + 1) / 2));
+        else
+            System.out.println("The number " + n + " is not a natural number");
+    }
+}
