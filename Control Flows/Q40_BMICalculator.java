95b4c43efbf79434ef69bca2b78bd28a5174c5bf
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-06-16 10:34:23 +0530

    {Ayush Gupta} add : Q40_BMICalculator.java

--- Control Flows/Q40_BMICalculator.java
+++ Control Flows/Q40_BMICalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q40_BMICalculator {
+class Q40_BMICalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double weight = sc.nextDouble();
