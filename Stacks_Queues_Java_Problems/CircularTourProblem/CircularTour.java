804e5ce8a4ba086efa4c794c24afbfb5b7e37dcb
Ayush Gupta <116937826+ayush-gupta456@users.noreply.github.com>
2025-07-07 12:38:46 +0530

    [Ayush Gupta] Add: Circular Tour Problem using Java

---
+++ Stacks_Queues_Java_Problems/CircularTourProblem/CircularTour.java
@@ -0,0 +1,24 @@
+package CircularTourProblem;
+
+public class CircularTour {
+    static class PetrolPump {
+        int petrol, distance;
+        PetrolPump(int p, int d) {
+            petrol = p;
+            distance = d;
+        }
+    }
+
+    public int findStartPoint(PetrolPump[] pumps) {
+        int start = 0, deficit = 0, balance = 0;
+        for (int i = 0; i < pumps.length; i++) {
+            balance += pumps[i].petrol - pumps[i].distance;
+            if (balance < 0) {
+                start = i + 1;
+                deficit += balance;
+                balance = 0;
+            }
+        }
+        return (balance + deficit >= 0) ? start : -1;
+    }
+}
